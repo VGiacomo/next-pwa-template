a2b80ee8a68fac8d044bb717a4e9c62155149dcf
Giacomo Viscido <giacomo@ouat.eu>
2022-12-11 17:26:35 +0100

    can now see the content of every email as innerhtml

--- pages/inbox.tsx
+++ pages/inbox.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Message, HomeProps } from './types'
 import { authorize, listMessages } from '../services/gmailService'
+import { atob, Buffer } from 'buffer'
 
 export async function getServerSideProps() {
 	let messages: Message[] = []
@@ -20,15 +21,35 @@ export async function getServerSideProps() {
 	}
 }
 
+function base64DecodeUnicode(str: any) {
+	// Convert Base64 encoded bytes to percent-encoding, and then get the original string.
+	// Decode the base64-encoded string
+  const decodedString = Buffer.from(str, 'base64').toString('utf8');
+	return decodedString
+}
+
 function Inbox({ emails }: HomeProps) {
 	console.log(emails)
+	let decodedString: string = ''
 
 	return (
 		<div>
 			List of emails :
-			<ul>
+			{/* <ul>
 				{emails.map((email) => (
 					<li key={email.id}>{email.snippet}</li>
+				))}
+			</ul> */}
+			<ul>
+				{emails.map((email) => (
+					<li key={email.id} style={{backgroundColor: 'white', width: 'auto'}}>
+						<div dangerouslySetInnerHTML={{ __html: base64DecodeUnicode(email.payload.parts[1].body.data) }}></div>
+						
+						{' '}
+						---------
+						-----------
+						{' '}
+					</li>
 				))}
 			</ul>
 		</div>
